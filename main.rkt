bf3bea5e60aa85a667743c7c0bd3407a02274da3
Antigen-1 <official@antigen.top>
2022-06-23 14:38:54 +0800

    init

---
+++ main.rkt
@@ -0,0 +1,20 @@
+#lang racket/base
+(provide table compile-pattern)
+
+(define table (make-hash))
+
+(define compile-pattern
+  (lambda (bytes)
+    (let ((port (open-input-bytes bytes))
+          (len (bytes-length bytes)))
+      (let loop ((state 0) (byte (read-byte port)))
+        (cond ((= state len) (void))
+              (else
+               (hash-set! table (cons state byte) (add1 state))
+               (let work ((index 1))
+                 (define v (- state index))
+                 (cond ((<= v 0) (void))
+                       ((bytes=? (subbytes bytes 0 v) (subbytes bytes index v))
+                        (hash-set! table (cons state (bytes-ref bytes v)) (add1 v))
+                        (work (add1 index)))))
+               (loop (add1 state) (read-byte port))))))))
